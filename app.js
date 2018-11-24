975a75c03970dacd5522b0a0f5153efbabe34740
balo11044 <balo11044@gmail.com>
2018-11-24 13:57:45 +0700

    make it better

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ io.on('connection', function (socket) {
     socket.emit('message-sender', data)
   });
   setInterval(function () {
-    socket.emit('sleepy', "doSomeThingFunny !")
+    socket.emit('sleepy', "doSomeThingFunny!")
   }, 20000)
   socket.on('stayWithMe', (data) => console.log(data))
 });
