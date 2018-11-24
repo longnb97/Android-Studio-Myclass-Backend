1763b39e849848f9923bbbcae80336bdc26d73b9
balo11044 <balo11044@gmail.com>
2018-11-24 14:00:54 +0700

    dsdsdsdsd
    sdsds

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ let server = http.createServer(app);
 
 const io = require('socket.io').listen(server);  //pass a http.Server instance
 
+const path = require('path');
+const INDEX = path.join(__dirname, 'index.html');
+
+
 const cors = require('cors');
 
 app.use(cors());
@@ -30,6 +34,6 @@ io.on('connection', function (socket) {
 app.get('/', home);
 
 function home(req, res) {
-  res.sendFile("INDEX");
+  res.sendFile(INDEX);
 }
 
