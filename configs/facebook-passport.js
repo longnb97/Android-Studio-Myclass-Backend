8efc761114cab6eb2d1ea2cd48ca0fc569e3fed2
balo11044 <balo11044@gmail.com>
2018-11-29 03:41:12 +0700

    sdsd

--- configs/facebook-passport.js
+++ configs/facebook-passport.js
@@ -1,4 +1,22 @@
 const passport = require('passport');
 const FacebookStrategy = require('passport-facebook').Strategy;
 
-const
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+})
+passport.deserializeUser((id, done) => {
+    //db query
+})
+
+
+passport.use(new FacebookStrategy(
+    {
+        clientID: process.env.FACEBOOK_CLIENT_ID,
+        clientSecret: process.env.FACEBOOK_APP_SECRET,
+        callbackURL: "https://loginfbapi.herokuapp.com/auth/fb/cb"
+    },
+    (accessToken, refreshToken, profile, done) => {
+        //db query
+    }
+))
